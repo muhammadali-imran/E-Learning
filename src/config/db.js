a87e1da478b04117b542f6eb816f2cbe8ba7fe54
muhammadali-imran <imranmuhammadali633@gmail.com>
2026-06-29 11:42:34 +0500

    Added some configurations settings

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'postgres',
+    logging: false,
+  }
+);
+
+export { sequelize };
